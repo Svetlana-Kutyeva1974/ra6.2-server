736df2224ef5b8f1894dbcefdccb1339b29927ad
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2022-05-18 01:38:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ router.delete('/notes/:id', async(ctx, next) => {
 
 app.use(router.routes()).use(router.allowedMethods());
 
-const port = process.env.PORT || 7077;
+const port = process.env.PORT || 7777;
 const server = http.createServer(app.callback());
 server.listen(port, () => console.log('server started on port', port));
